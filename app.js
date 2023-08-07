c919fa36351d17795a127bca3b1c875688b50ad2
seena <iamsrinivas230@gmail.com>
2023-08-07 17:39:25 +0530

    updated code

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 const itemWrappers = document.querySelectorAll(".item_wrapper");
 const inputClick = document.querySelectorAll(".item_wrapper_individual");
+const totalTxtElement = document.getElementById("total_txt");
 
 itemWrappers.forEach((itemWrapper) => {
   itemWrapper.addEventListener("click", (e) => {
-    const radioButtonEnable = e.target.querySelector('input[type="radio"]');
-    const enableValue = e.target.querySelector(".size_clr_section");
+    const radioButtonEnable = e.currentTarget.querySelector(
+      'input[type="radio"]'
+    );
+    const enableValue = e.currentTarget.querySelector(".size_clr_section");
     const hideAll = document.querySelectorAll(".size_clr_section");
     const hideBorder = document.querySelectorAll(".item_wrapper");
-    hideAll.forEach((element) => {
-      element.classList.add("section_hide");
-    });
+    const priceElement = e.currentTarget.querySelector("h4");
+
+    if (priceElement) {
+      const price = parseFloat(priceElement.textContent.replace("DKK ", ""));
+      total = price;
+      updateTotalValue();
+    }
 
     hideBorder.forEach((hide) => {
       hide.classList.remove("content_active");
     });
 
-    radioButtonEnable.checked = true;
+    hideAll.forEach((element) => {
+      element.classList.add("section_hide");
+    });
+
+    if (radioButtonEnable) {
+      radioButtonEnable.checked = true;
+    }
+
     enableValue.classList.remove("section_hide");
-    itemWrapper.classList.add("content_active");
+
+    if (e.currentTarget) {
+      e.currentTarget.classList.add("content_active");
+    }
   });
 });
 
@@ -29,3 +46,9 @@ inputClick.forEach((itemWrapper) => {
     clickedInput.dispatchEvent(clickEvent);
   });
 });
+
+//To update the pricing values
+let total = 0;
+function updateTotalValue() {
+  totalTxtElement.textContent = `Total : DKK ${total.toFixed(2)}`;
+}
